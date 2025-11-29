e092184107c89e152d27e3739962f1620d00c613
Thomas Roß <pzycho1980@gmail.com>
2025-11-30 00:51:09 +0100

    Added support for DDS R16F + R32F files through Pfim

---
+++ PfimImageLoader.cs
@@ -0,0 +1,60 @@
+﻿using Pfim;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace TinyView
+{
+    public class PfimImageLoader
+    {
+        public static (IRawImageDataProvider, WriteableBitmap) LoadImage(string path)
+        {
+            using var image = Pfimage.FromFile(path);
+                
+            if (image.Format != ImageFormat.R16f && image.Format != ImageFormat.R32f)
+                throw new InvalidOperationException("Expected a 16/32-bit grayscale image.");
+
+            int width = image.Width;
+            int height = image.Height;
+            bool isHalf = image.Format == ImageFormat.R16f;
+
+            // extract raw data
+            var pixelData = new float[width, height];
+
+            for (int y = 0; y < height; ++y)
+            {
+                for (int x = 0; x < width; ++x)
+                {
+                    float value = isHalf ? ((float)BitConverter.ToHalf(image.Data, (y * width + x) * 2))
+                                            : BitConverter.ToSingle(image.Data, (y * width + x) * 4);
+                    pixelData[x, y] = value;
+                }
+            }
+
+            var rawData = new RawImageData<float>(width, height, pixelData);
+
+            // convert to 8-bit grayscale for display
+            byte[] pixels8 = new byte[width * height];
+
+            float min = pixelData.Cast<float>().Min();
+            float max = pixelData.Cast<float>().Max();
+            float scale = min == max ? 1f : 255f / (max - min);
+
+            for (int y = 0; y < height; ++y)
+            {
+                for (int x = 0; x < width; ++x)
+                {
+                    float norm = (pixelData[x, y] - min) * scale;
+                    byte scaled = (byte)Math.Clamp(norm, 0, 255);
+                    pixels8[y * width + x] = scaled;
+                }
+            }
+
+            // create a WPF bitmap
+            var bitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Gray8, null);
+            bitmap.WritePixels(new Int32Rect(0, 0, width, height), pixels8, width, 0);
+
+            return (rawData, bitmap);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # TinyView
-A quick and dirty slapped together viewer for 16-bit grayscale (single channel) PNG image files.
+A specialized viewer for 16+32bit single channel images.
+- PNG: 16-bit grayscale
+- DDS: R16F, R32F
+
 The value range between min and max from the image will be scaled to 0-255 range for screen display while the mouse over will display the raw value in the status bar.
 
 ## Build
 Visual Studio 2022
 
 ## Dependencies
-Magick.Net as NuGet package
+- [Magick.Net](https://github.com/dlemstra/Magick.NET) for PNG file loading
+- [Pfim](https://github.com/nickbabcock/Pfim) for DDS file loading
