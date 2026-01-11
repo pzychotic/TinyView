6b88cc31fef1770153333aa2f5da9ea9bb2607a6
Thomas Roß <pzycho1980@gmail.com>
2026-01-11 15:14:51 +0100

    [App] Forcing invariant culture

--- Source/TinyView/App.xaml.cs
+++ Source/TinyView/App.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System.Globalization;
 using System.Windows;
 
 namespace TinyView
@@ -9,6 +8,19 @@ namespace TinyView
     /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var invariant = CultureInfo.InvariantCulture;
 
+            // Ensure the current thread uses invariant culture
+            Thread.CurrentThread.CurrentCulture = invariant;
+            Thread.CurrentThread.CurrentUICulture = invariant;
+
+            // Ensure future threads default to invariant culture
+            CultureInfo.DefaultThreadCurrentCulture = invariant;
+            CultureInfo.DefaultThreadCurrentUICulture = invariant;
+
+            base.OnStartup(e);
+        }
+    }
 }
