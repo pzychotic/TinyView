eff63f0085fd07fc328f6adc2b5ce8ecf822a5b4
Thomas Roß <pzycho1980@gmail.com>
2026-02-24 20:40:57 +0100

    [Settings] Changed window position defaults to NaN
    This better denotes yet unspecified values.

--- Source/TinyView/Models/UserSettings.cs
+++ Source/TinyView/Models/UserSettings.cs
@@ -5,8 +5,8 @@ namespace TinyView.Models
         // Window size and position
         public double Width { get; set; } = 800;
         public double Height { get; set; } = 600;
-        public double Left { get; set; } = 0;
-        public double Top { get; set; } = 0;
+        public double Left { get; set; } = double.NaN;
+        public double Top { get; set; } = double.NaN;
 
         // Whether the window was maximized when the app exited
         public bool IsMaximized { get; set; } = false;
