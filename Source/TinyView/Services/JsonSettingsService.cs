be600ef374d596a411eaf2308e0843548c2ad2ec
Thomas Roß <pzycho1980@gmail.com>
2026-02-27 00:17:31 +0100

    [Settings] Make settings more robust and testable

--- Source/TinyView/Services/JsonSettingsService.cs
+++ Source/TinyView/Services/JsonSettingsService.cs
@@ -1,25 +1,42 @@
 using System.IO;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using TinyView.Models;
 
 namespace TinyView.Services
 {
     public sealed class JsonSettingsService : ISettingsService
     {
-        private static readonly string SettingsDir =
+        private static readonly string DefaultSettingsDir =
             Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "TinyView");
 
-        private static readonly string SettingsPath =
-            Path.Combine(SettingsDir, "UserSettings.json");
+        private static readonly JsonSerializerOptions JsonOptions = new()
+        {
+            NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals
+        };
+
+        private readonly string _settingsDir;
+        private readonly string _settingsPath;
+
+        public JsonSettingsService()
+            : this(DefaultSettingsDir)
+        {
+        }
+
+        public JsonSettingsService(string settingsDir)
+        {
+            _settingsDir = settingsDir;
+            _settingsPath = Path.Combine(settingsDir, "UserSettings.json");
+        }
 
         public UserSettings? Load()
         {
             try
             {
-                if (File.Exists(SettingsPath))
+                if (File.Exists(_settingsPath))
                 {
-                    var txt = File.ReadAllText(SettingsPath);
-                    return JsonSerializer.Deserialize<UserSettings>(txt);
+                    var txt = File.ReadAllText(_settingsPath);
+                    return JsonSerializer.Deserialize<UserSettings>(txt, JsonOptions);
                 }
             }
             catch
@@ -34,9 +51,9 @@ namespace TinyView.Services
         {
             try
             {
-                Directory.CreateDirectory(SettingsDir);
-                var txt = JsonSerializer.Serialize(settings);
-                File.WriteAllText(SettingsPath, txt);
+                Directory.CreateDirectory(_settingsDir);
+                var txt = JsonSerializer.Serialize(settings, JsonOptions);
+                File.WriteAllText(_settingsPath, txt);
             }
             catch
             {
