9fed0fb1d92c8153e5ab7ea4a6c194cde6b67dc9
Thomas Roß <pzycho1980@gmail.com>
2026-05-22 20:09:31 +0200

    [Modernize] Use modern collection expressions for member intialization

--- Source/TinyView/Models/ColorPalettes.cs
+++ Source/TinyView/Models/ColorPalettes.cs
@@ -1,44 +1,36 @@
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
-namespace TinyView.Models
+namespace TinyView.Models;
+
+public static class ColorPalettes
 {
-    public static class ColorPalettes
+    public readonly record struct PaletteEntry(string Name, BitmapPalette Palette);
+
+    public static readonly IReadOnlyList<PaletteEntry> Palettes =
+    [
+        new("Gray", BitmapPalettes.Gray256),
+
+        // color maps
+        CreateEntry("Magma", ColorMaps.Magma),
+        CreateEntry("Inferno", ColorMaps.Inferno),
+        CreateEntry("Plasma", ColorMaps.Plasma),
+        CreateEntry("Viridis", ColorMaps.Viridis),
+        CreateEntry("Cividis", ColorMaps.Cividis),
+        CreateEntry("Rocket", ColorMaps.Rocket),
+        CreateEntry("Mako", ColorMaps.Mako),
+        CreateEntry("Turbo", ColorMaps.Turbo)
+    ];
+
+    private static PaletteEntry CreateEntry(string name, byte[,] colors)
     {
-        public readonly record struct PaletteEntry(string Name, BitmapPalette Palette);
-
-        public static readonly IReadOnlyList<PaletteEntry> Palettes;
-
-        static ColorPalettes()
+        Color[] paletteColors = new Color[256];
+        for (int i = 0; i < 256; ++i)
         {
-            var list = new List<PaletteEntry>
-            {
-                new("Gray", BitmapPalettes.Gray256),
-
-                // color maps
-                CreateEntry("Magma", ColorMaps.Magma),
-                CreateEntry("Inferno", ColorMaps.Inferno),
-                CreateEntry("Plasma", ColorMaps.Plasma),
-                CreateEntry("Viridis", ColorMaps.Viridis),
-                CreateEntry("Cividis", ColorMaps.Cividis),
-                CreateEntry("Rocket", ColorMaps.Rocket),
-                CreateEntry("Mako", ColorMaps.Mako),
-                CreateEntry("Turbo", ColorMaps.Turbo)
-            };
-
-            Palettes = list.AsReadOnly();
+            paletteColors[i] = Color.FromRgb(colors[i, 0], colors[i, 1], colors[i, 2]);
         }
+        var palette = new BitmapPalette(paletteColors);
 
-        static PaletteEntry CreateEntry(string name, byte[,] colors)
-        {
-            Color[] paletteColors = new Color[256];
-            for (int i = 0; i < 256; ++i)
-            {
-                paletteColors[i] = Color.FromRgb(colors[i, 0], colors[i, 1], colors[i, 2]);
-            }
-            var palette = new BitmapPalette(paletteColors);
-
-            return new PaletteEntry(name, palette);
-        }
+        return new PaletteEntry(name, palette);
     }
 }
